a7e670ce9be798af546b5c4081e69a208b6679be
Yovany Angeles <yolk7979@hotmail.com>
2018-08-10 15:32:43 -0500

    se resuelven conflictos en routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,7 +19,6 @@ const routes: Routes = [
   },
   { path: 'historial', component: HistoryComponent },
   { path: 'pedidos', component: PedidoschefComponent },
-  { path: 'historial', component: HistoryComponent },
   { path: 'chefboard', component: ChefboardComponent }
 ];
 
